6b6f9c4739453cfd616f2868e6799a1dca461d13
Lakshman Gurung <gurunglaxman0@gmail.com>
2023-12-12 12:39:24 +0530

    FeedItem and FeedLoader protocol boundary added

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,14 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Lakshman Gurung on 12/12/23.
+//
+
+import Foundation
+struct FeedItem {
+    let id: UUID
+    let descripion: String?
+    let locastion: String?
+    let image: URL
+}
---
+++ EssentialFeed/FeedLoader.swift
@@ -0,0 +1,17 @@
+//
+//  FeedLoader.swift
+//  EssentialFeed
+//
+//  Created by Lakshman Gurung on 12/12/23.
+//
+
+import Foundation
+
+enum FeedLoaderResult {
+    case success([FeedItem])
+    case error(Error)
+}
+
+protocol FeedLoader {
+    func load(completion:@escaping (FeedLoaderResult) -> Void)
+}
