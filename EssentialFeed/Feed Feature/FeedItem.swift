734463c35f8590ccbd0a292d7936e05b9342adce
Lakshman Gurung <gurunglaxman0@gmail.com>
2023-12-31 01:41:02 +0530

    FeedItem fields typoerror fixed

--- EssentialFeed/Feed Feature/FeedItem.swift
+++ EssentialFeed/Feed Feature/FeedItem.swift
@@ -7,8 +7,15 @@
 
 import Foundation
 public struct FeedItem: Equatable {
-    let id: UUID
-    let descripion: String?
-    let locastion: String?
-    let imageURL: URL
+    public let id: UUID
+    public let description: String?
+    public let location: String?
+    public let imageURL: URL
+    
+    public init(id: UUID, description: String?, location: String?, imageURL: URL) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.imageURL = imageURL
+    }
 }
